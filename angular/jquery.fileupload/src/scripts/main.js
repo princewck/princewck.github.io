4b515651baa36c0b99b9a51e06e32e95798bf778
chengkai.wang <trickle.wang@petkit.com>
2016-10-19 17:02:14 +0800

    init

--- angular/jquery.fileupload/src/scripts/main.js
+++ angular/jquery.fileupload/src/scripts/main.js
@@ -2,13 +2,13 @@ require.config({
     baseUrl:'../src',
     waitSeconds:0,
     paths: {
-        "angular":"http://cdn.bootcss.com/angular.js/1.4.6/angular.min",
+        "angular":"//cdn.bootcss.com/angular.js/1.4.6/angular.min",
         'angular-route':"http://cdn.bootcss.com/angular.js/1.4.6/angular-route.min",
         'angular-require': "public/angular-require/angular-require.min",
         'bootstrap':"//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min",
-        "jquery":"http://cdn.bootcss.com/jquery/2.1.4/jquery.min",
-        'jquery.fileupload':'http://cdn.bootcss.com/blueimp-file-upload/9.10.4/jquery.fileupload',
-        'jquery.ui.widget':'http://cdn.bootcss.com/blueimp-file-upload/9.10.4/vendor/jquery.ui.widget',
+        "jquery":"//cdn.bootcss.com/jquery/2.1.4/jquery.min",
+        'jquery.fileupload':'//cdn.bootcss.com/blueimp-file-upload/9.10.4/jquery.fileupload',
+        'jquery.ui.widget':'//cdn.bootcss.com/blueimp-file-upload/9.10.4/vendor/jquery.ui.widget',
         'app':"scripts/app",
         'directive.fileupload':'directives/fileupload/uploader'
     },
@@ -26,7 +26,7 @@ require.config({
     },
     map: {
         '*': {
-            'css': 'http://apps.bdimg.com/libs/require-css/0.1.8/css.min.js' // or whatever the path to require-css is
+            'css': '//apps.bdimg.com/libs/require-css/0.1.8/css.min.js' // or whatever the path to require-css is
         }
     }
 });
