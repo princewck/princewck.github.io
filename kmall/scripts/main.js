c2e34d3381812ecb5ae848d0a4a5524ee781b5a3
chengkai.wang <trickle.wang@petkit.com>
2017-01-04 10:49:50 +0800

    kmall fix

--- kmall/scripts/main.js
+++ kmall/scripts/main.js
@@ -9,7 +9,7 @@ require.config({
         'angular-moment': BOWER_DIR + '/angular-moment/angular-moment.min',
         'angular-require': BOWER_DIR + '/angular-require/angular-require.min',
         'angular-ui-router': BOWER_DIR + '/angular-ui-router/release/angular-ui-router.min',
-        'angular-cookie': "http://cdn.bootcss.com/angular.js/1.4.6/angular-cookies.min",
+        'angular-cookie': "//cdn.bootcss.com/angular.js/1.4.6/angular-cookies.min",
         'jquery': BOWER_DIR + '/jquery/dist/jquery.min',
         'moment': BOWER_DIR + '/moment/min/moment.min',
         //项目相关
@@ -25,7 +25,7 @@ require.config({
     },
     map: {
         '*': {
-            'css': 'http://apps.bdimg.com/libs/require-css/0.1.8/css.min.js' // or whatever the path to require-css is
+            'css': '//apps.bdimg.com/libs/require-css/0.1.8/css.min.js' // or whatever the path to require-css is
         }
     },
     shim: {
--- kmall/src/directives/common_pages/site-footer.js
+++
@@ -1,8 +0,0 @@
-define(['app', 'css!directives/common_pages/site-footer.css'], function(app) {
-    app.directive('siteFooter', function() {
-        return {
-            restrict: 'A',
-            templateUrl: 'directives/common_pages/site-footer.html'
-        }
-    });
-});
